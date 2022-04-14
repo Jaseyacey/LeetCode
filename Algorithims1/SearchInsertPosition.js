2762e7015a78fd02996871d9d90aee0a7578cf7d
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2022-04-14 15:41:43 +0100

    Update SearchInsertPosition.js

--- Algorithims1/SearchInsertPosition.js
+++ Algorithims1/SearchInsertPosition.js
@@ -8,25 +8,18 @@
  * @param {number} target
  * @return {number}
  */
- var searchInsert = function(nums, target) {
-        // if the array is empty, return 0
-        if(nums.length === 0) return 0;
-        // if the target is the first element, return 0
-        if(nums.length === 1) return nums[0] === target ? 0 : 1;
-        let left =  0;
-        let right = nums.length -1;
-        // while the left is less than the right
-        while(left <= right){
-        // if the target is the first element, return 0
-            let mid = Math.floor((left + right)/2);
-        // if the target is the first element, return 0
-            if (nums[mid] === target) return mid;
-            if (nums[mid] < target) {
-            left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length;
+    while(left < right) {
+        mid=Math.floor((left+right)/2);
+        if(nums[mid]===target) return mid;
+        else if(nums[mid]<target) left = mid+1;
+        else {
+            right = mid - 1;
         }
-        // if the target is not found, return the index where it would be if it were inserted
-        return left;
-    };
+        nums.push(target);
+        let res = nums.sort(function(a,b){return a-b});
+        return res.indexOf(target);
+    }
+};
