a92b318e496fbbb33deab8529dcf0f819c9f710a
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2022-04-19 20:59:01 +0100

    getting the median number of a sorted array

---
+++ Algorithims1/TwoSortedArrays.js
@@ -0,0 +1,28 @@
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+ * return the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n))
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+    var findMedianSortedArrays = function(nums1, nums2) {
+        // create a new array
+        let nums = nums1.concat(nums2);
+        // sort the array
+        nums.sort((a,b) => a-b);
+        // if the length of the array is odd
+        let length = nums.length;
+        // if the length is odd
+        if(length%2==0) {
+        // return the average of the two middle numbers
+            return (nums[length/2-1]+nums[length/2])/2;
+        } else {
+            // return the middle number
+            return nums[Math.floor(length/2)]
+        }
+    };
+
--- Algorithims1/reverseAWord.js
+++ Algorithims1/reverseAWord.js
@@ -6,5 +6,4 @@
 var reversewords = function(s) {
     console.log("s: ", s)
     return s.split(' ').map(w => w.split('').reverse().join('')).join(' ');
-
 };
