3a0a84500692f97670f80b659954ecff0ab34037
Jaseyacey <41111290+Jaseyacey@users.noreply.github.com>
2022-04-20 19:21:59 +0100

    Changed the approach to find the middle of a list

--- Algorithims1/MiddleLinkedList.js
+++ Algorithims1/MiddleLinkedList.js
@@ -9,15 +9,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var middleNode = function(head) {
+ var removeNthFromEnd = function(head, n) {
      // create a dummy node
-    let fast = slow = head;
-    // while fast is not null
-    while (fast && fast.next) {
-    // move fast to the next node
-        fast = fast.next.next;
-    // move slow to the next node
-        slow = slow.next;
-    }
-    return slow;
+     let dummy = new ListNode(0);
+        dummy.next = head;
+        // create a pointer to the first node
+        let first = dummy;
+        // create a pointer to the second node
+        let second = dummy;
+        // move the second pointer n nodes ahead
+        for(let i = 0; i < n; i++) {
+            second = second.next;
+        } // now second is n nodes ahead of first
+        // move the first pointer until second reaches the end
+        while(second.next) {
+            first = first.next;
+            second = second.next;
+        }
+        // now first is n+1 nodes ahead of the node to be removed
+        // set the next of the first node to be the next of the node to be removed
+        first.next = first.next.next;
+        return dummy.next;
 };
